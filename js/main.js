8e52983c30eca4c01826573f15e34f4aa0250077
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-04-19 17:36:22 +0200

    need help2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
-let insertImg = document.getElementsByClassName("dimImg");
-let x = document.createElement("img");
-x.src = "../img/01.webp";
-x.className = "slide hidden";
-insertImg.append(x);
+let insertImg = document.getElementById("insertImg");
+let imgs = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"]
+let btnPrevious = document.getElementById("previous");
+let btnNext = document.getElementById("next");
+let currentSlide = 0;
+
+for (let i = 0; i < imgs.length; i++) {
+    let img = imgs[i];
+    let x = document.createElement("img");
+    x.src = img ;
+    x.className = "slide";
+    insertImg.append(x);
+    if (i != 0) {
+        x.className = "slide hidden";
+    }
+}
+let slideElements = document.getElementsByClassName("slide");
+
+btnNext.addEventListener("click", function(){
+
+    console.log("Current Slide: ", currentSlide);
+
+    if(currentSlide < imgs.length-1) {
+        for (let c = 0; c < imgs.length; c++) {
+            const slide = slideElements[c];
+
+            if(c == currentSlide+1 ) {
+                slide.classList.remove("hidden");
+            } else {
+                slide.classList.add("hidden");
+            }
+        }
+        
+        currentSlide++;
+    } else {
+        currentSlide = 0
+        slideElements[ currentSlide ]
+    }
+    
+});
