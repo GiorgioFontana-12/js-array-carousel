906eaf8599ba33d2943781151b6224a43cddba73
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-04-20 09:36:21 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -33,8 +33,40 @@ btnNext.addEventListener("click", function(){
         
         currentSlide++;
     } else {
+        let lastSlide = slideElements [imgs.length-1]
+        lastSlide.classList.add("hidden");
+        let firstSlide =  slideElements [0]
+        firstSlide.classList.remove("hidden");
         currentSlide = 0
-        slideElements[ currentSlide ]
+
+
     }
     
+});
+
+btnPrevious.addEventListener("click", function(){
+
+    console.log("Current Slide: ", currentSlide);
+
+
+    if(currentSlide>0) {
+        for (let c = 0; c < slideElements.length; c++) {
+                const slide = slideElements[c];
+
+            if(c == currentSlide-1 ) {
+                slide.classList.remove("hidden");
+            } else {
+                slide.classList.add("hidden");
+            }
+        }
+        
+        currentSlide--;
+    } else {
+        let lastSlide = slideElements [imgs.length-1]
+        let firstSlide =  slideElements [0]
+        firstSlide.classList.add("hidden");
+        lastSlide.classList.remove("hidden");
+        currentSlide = imgs.length-1
+    }
+
 });
